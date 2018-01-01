ea40dfba4c882b66645f0cfa8f6ea45e52cb67ad
Derya O. Kurin <dozdemir663@gmail.com>
2018-01-01 17:21:16 +0000

    tenth commit"

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -4,7 +4,7 @@ import './SearchBar.css';
 class SearchBar extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { term: ""};
+		this.state = { term:""};
 		this.search = this.search.bind(this);
 		this.handleTermChange = this.handleTermChange.bind(this);
 }
@@ -15,7 +15,7 @@ class SearchBar extends React.Component {
 	}
 
 	handleTermChange(event) {
-		this.search({term: event.target.value});
+		this.setState({term: event.target.value});
 	}
 
 	render() {
